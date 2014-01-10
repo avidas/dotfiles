b26720432fd28cb182c057f7dde105618a018a40
Avi Das <adas@colgate.edu>
2014-01-10 10:08:36 -0600

    Add script to create symlinks from home directory to dotfiles

---
+++ makesymlinks.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This script creates symlinks from the home dir to any desired dotfiles in ~/dotfiles. Fork of @michaeljsmalley's version
+
+dir=~/dotfiles
+olddir=~/dotfiles_old
+files="bashrc bash_aliases bash_profile tmux.conf vimrc"
+
+echo -n "Create $olddir for backup of existing dotfiles in ~ ..."
+mkdir -p $olddir
+echo "done"
+
+echo -n "Changing to $dir directory ..."
+cd $dir
+echo "done"
+
+for file in $files; do
+  echo "Moving existing dotfiles from ~ to $olddir"
+  mv ~/.$file ${olddir}/
+  echo "Creating symlink to $file in home dir."
+  ln -s ${dir}/${file} ~/.$file
+done
+
+
+
